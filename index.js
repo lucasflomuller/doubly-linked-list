5c98f34d56165b60f4203e3df24dc9934597efc1
lucasflomuller <lucasflorianomuller@gmail.com>
2019-12-04 23:23:23 -0300

    feat: Add DLL constructor

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Doubly linked list
+class DoublyLinkedList {
+  constructor(value) {
+    // Inicializa a lista já com um valor
+    this.head = {
+      value: value,
+      next: null,
+      prev: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+
+let myLinkedList = new DoublyLinkedList(10);
